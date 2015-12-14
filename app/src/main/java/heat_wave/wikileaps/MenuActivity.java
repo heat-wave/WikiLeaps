978d488b4431557b85782aac6a39ee5c24c892e3
heat_wave <allpicksucks@gmail.com>
2015-12-15 02:53:20 +0300

    Now handling orientation changes in menu, too.

--- app/src/main/java/heat_wave/wikileaps/MenuActivity.java
+++ app/src/main/java/heat_wave/wikileaps/MenuActivity.java
@@ -3,11 +3,11 @@ package heat_wave.wikileaps;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Typeface;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.ViewFlipper;
 
 import heat_wave.wikileaps.utils.Animations;
@@ -18,6 +18,7 @@ import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 public class MenuActivity extends Activity {
 
     public static final String DIFFICULTY = "difficulty";
+    public static final String TAG = "wiki_leaps";
     private ViewFlipper flipper;
 
     @Override
@@ -26,6 +27,17 @@ public class MenuActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
         flipper = (ViewFlipper)findViewById(R.id.flipper);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            Log.d(TAG, "Switched to landscape");
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            Log.d(TAG, "Switched to portrait");
+        }
     }
 
     public void startGame(View v) {
